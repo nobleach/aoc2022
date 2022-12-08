02f229344dfbfd7ff9b6694291f083aaa572127a
Jim Wharton <jamesawharton@gmail.com>
2022-12-07 21:20:36 -0700

    chore: get FS building

--- app/src/main/kotlin/aoc2022/Day07.kt
+++ app/src/main/kotlin/aoc2022/Day07.kt
@@ -10,36 +10,38 @@ class Directory(name: String, val children: MutableList<Node>, parent: Directory
 
 class Day07 {
     fun part01(input: String): Int {
-        val MAX_SIZE = 100_000
+        // val MAX_SIZE = 100_000
         val root = Directory("/", mutableListOf(), null)
         var currentNode = root
 
         input.trimMargin().split("\n")
-            .drop(1) // we're already in /
-            .forEach {
-                with(it) {
-                    when {
-                        // It's a command
-                        startsWith("$ cd") -> {
-                            when (it.split(" ").last()) {
-                                ".." -> currentNode = currentNode.parent
-                                else -> currentNode = currentNode.children.find(child.name == it.split(" ").last()),
-                            }
+        .drop(1) // we're already in /
+        .forEach {
+            with(it) {
+                when {
+                    // It's a command
+                    startsWith("$ cd") -> {
+                        if (it == "$ cd ..") {
+                            currentNode = currentNode.parent!!
+                        } else {
+                            currentNode = currentNode.children.find {
+                                child -> child.name == it.split(" ").last()
+                            } as Directory
                         }
-                        startsWith("$ ls") -> println("listing")
-                        startsWith("dir") -> currentNode.children.add(Directory(it.split(" ").last(), mutableListOf(), currentNode))
                     }
-                        else -> currentNode.children.add(
-                            File(
-                                it.split(" ").last(),
-                                it.split(" ").first().toInt(),
-                                currentNode
-                            ))
+                    startsWith("$ ls") -> println("listing")
+                    startsWith("dir") -> currentNode.children.add(Directory(it.split(" ").last(), mutableListOf(), currentNode))
+                    else -> currentNode.children.add(
+                        File(
+                            it.split(" ").last(),
+                            it.split(" ").first().toInt(),
+                            currentNode
+                        ))
                     }
                 }
-                println(root)
-                return 3
-        }
+            }
+        println(root)
+        return 3
     }
 
     /* fun part02(input: String): Int {
